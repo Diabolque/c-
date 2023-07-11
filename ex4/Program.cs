f5b0cc635936df0fdefc0676a26cf1806336e387
kaidan <aagusashvili@gmail.com>
2023-07-11 16:04:15 +0300

    all

---
+++ ex4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите три числа через пробел:");
+string input = Console.ReadLine();
+string[] numbers = input.Split(' ');
+Console.WriteLine("Введеные числа: {0} {1} {2}", numbers[0], numbers[1], numbers[2]);
+if ((int.Parse(numbers[0]) > int.Parse(numbers[1])) && (int.Parse(numbers[0]) > int.Parse(numbers[2])))
+    Console.WriteLine("Наибольшее число: {0}", numbers[0]);
+else if ((int.Parse(numbers[1]) > int.Parse(numbers[0])) && (int.Parse(numbers[1]) > int.Parse(numbers[2])))
+    Console.WriteLine("Наибольшее число: {0}", numbers[1]);
+else
+    Console.WriteLine("Наибольшее число: {0}", numbers[2]);
+    
---
+++ ex8/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int N = int.Parse(input);
+Console.WriteLine("Введенное число: {0}", N);
+for (int i = 1; i < N; i++)
+    if(i%2==0)
+        Console.WriteLine("Четное число: {0}",i);
+        
